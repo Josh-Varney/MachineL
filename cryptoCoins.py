3a831a5d540763a400c15fb7f2566b15cb83e952
Joshua Varney <up2138017@myport.ac.uk>
2023-11-05 23:32:29 +0000

    Functional Programming

--- cryptoCoins.py
+++ cryptoCoins.py
@@ -1,8 +1,18 @@
 from pycoingecko import CoinGeckoAPI
 from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 cg = CoinGeckoAPI()
+
+def get_historical_data(coin_id, vs_currency, start_date, end_date):
+    start_timestamp = int(start_date.timestamp())
+    end_timestamp = int(end_date.timestamp())
+    historical_data = cg.get_coin_market_chart_range_by_id(id=coin_id, vs_currency=vs_currency, from_timestamp=start_timestamp, to_timestamp=end_timestamp)
+    prices = historical_data['prices']
+    timestamps = [datetime.utcfromtimestamp(timestamp[0]/1000).strftime('%Y-%m-%d %H:%M:%S') for timestamp in prices]
+    # Converts into seconds and converts into the following format
+    return timestamps, prices
 
 # Define the coin id and currency
 coin_id = 'bitcoin'
@@ -14,28 +24,8 @@ coin_id2 = 'ethereum'
 start_date = datetime(2023, 1, 1)
 end_date = datetime(2023, 1, 5)
 
-start_timestamp = int(start_date.timestamp())
-end_timestamp = int(end_date.timestamp())
-
-# Get historical market chart data
-historical_data = cg.get_coin_market_chart_range_by_id(id=coin_id, vs_currency=vs_currency, from_timestamp=start_timestamp, to_timestamp=end_timestamp)
-
-
-# Extracting prices and timestamps
-prices = historical_data['prices']
-timestamps = [datetime.utcfromtimestamp(timestamp[0]/1000).strftime('%Y-%m-%d %H:%M:%S') for timestamp in prices]
-                # Converts into seconds and converts into the following format
+timestamps, prices = get_historical_data(coin_id, vs_currency, start_date, end_date)           
                 
-# Print the results
-for timestamp, price in zip(timestamps, prices):
-    print(f"Date: {timestamp}, Price: {price[1]} {vs_currency}")
-    
-
-plt.plot(historical_data['prices'], label='Actual Prices')
-plt.title(f'{coin_id} Price Over Time')
-plt.xlabel('Date')
-plt.ylabel('Prices')
-plt.legend()
-plt.show()
-
-
+days = np.arange(1, len(timestamps) + 1)
+X = days.reshape(-1, 1)
+print(prices)
